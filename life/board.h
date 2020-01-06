a830b19d5162f817207ce8f3949580592f56d251
Bruno Belarte <bruno.belarte@gmail.com>
2020-01-06 21:50:02 +0000

    Read correct size from header

--- life/board.h
+++ life/board.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
 namespace life {
@@ -31,16 +32,76 @@ private:
 class RLEParser
 {
 public:
-	Board parse(const std::string& input) const
-	{
-		return Board{int(input[4] - '0'), int(input[11] - '0')};
+	RLEParser(const std::string& input) :
+		m_index{0},
+		m_lastInt{0},
+		m_input(input)
+	{}
+
+	std::unique_ptr<Board> parse() {
+		parseHeader();
+		return std::move(m_output);
 	}
+
+private:
+	void parseHeader()
+	{
+		if (!readString("x = ") || !readInt()) {
+			throw std::runtime_error("Header incorrect, cannot read width");
+		}
+		int width = m_lastInt;
+
+		if (!readString(", y = ") || !readInt()) {
+			throw std::runtime_error("Header incorrect, cannot read height");
+		}
+		int height = m_lastInt;
+
+		m_output = std::make_unique<Board>(width, height);
+
+		if (!readString("\n")) {
+			throw std::runtime_error("Header incorrect, missing new line");
+		}
+	}
+
+	bool readString(const std::string& s) {
+		for (unsigned int i=0; i<s.size(); ++i) {
+			if (s[i] != m_input[m_index]) {
+				return false;
+			}
+
+			++m_index;
+		}
+
+		return true;
+	}
+
+	bool readInt() {
+		m_lastInt = 0;
+		unsigned int start = m_index;
+
+		while (isDigit()) {
+			m_lastInt *= 10;
+			m_lastInt += m_input[m_index] - '0';
+			++m_index;
+		}
+
+		return m_index != start;
+	}
+
+	bool isDigit() const {
+		return m_input[m_index] >= '0' && m_input[m_index] <= '9';
+	}
+
+	unsigned int m_index;
+	int m_lastInt;
+	const std::string m_input;
+	std::unique_ptr<Board> m_output;
 };
 
-Board FromRLE(const std::string& pattern)
+std::unique_ptr<Board> FromRLE(const std::string& pattern)
 {
-	RLEParser parser;
-	return parser.parse(pattern);
+	RLEParser parser(pattern);
+	return parser.parse();
 }
 
 } // end namespace
--- life/tests/board.cpp
+++ life/tests/board.cpp
@@ -3,8 +3,8 @@
 
 TEST(BoardCreation, creates_with_correct_dimensions)
 {
-	auto board = life::FromRLE("x = 5, y = 6");
+	auto board = life::FromRLE("x = 15, y = 6\n");
 
-	ASSERT_EQ(board.width(), 5);
-	ASSERT_EQ(board.height(), 6);
+	ASSERT_EQ(15, board->width());
+	ASSERT_EQ(6, board->height());
 }

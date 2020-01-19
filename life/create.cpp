188ae77cbcb108286680b87b3d50166992847734
Bruno Belarte <bruno.belarte@gmail.com>
2020-01-19 12:05:07 +0000

    Trim size for single pattern with offset

--- life/create.cpp
+++ life/create.cpp
@@ -8,15 +8,17 @@ namespace life {
 std::unique_ptr<Board> Create(Input input, Generator gen)
 {
 	auto coords = gen(input.pattern);
+	auto xOffset = input.offset.x;
+	auto yOffset = input.offset.y;
 
 	unsigned int width = 0, height = 0;
 	unsigned int xOrigin = std::numeric_limits<unsigned int>::max();
 	unsigned int yOrigin = std::numeric_limits<unsigned int>::max();
 	for (const auto c : coords) {
-		xOrigin = std::min(xOrigin, c.x);
-		yOrigin = std::min(yOrigin, c.y);
-		width = std::max(width, c.x);
-		height = std::max(height, c.y);
+		xOrigin = std::min(xOrigin, c.x + xOffset);
+		yOrigin = std::min(yOrigin, c.y + yOffset);
+		width = std::max(width, c.x + xOffset);
+		height = std::max(height, c.y + yOffset);
 	}
 
 	auto board = std::make_unique<Board>(width+1-xOrigin, height+1-yOrigin);
--- life/tests/board.cpp
+++ life/tests/board.cpp
@@ -30,7 +30,7 @@ TEST_F(BoardCreation, creates_with_dimension_from_pattern)
 {
 	EXPECT_CALL(mockGenerator, create(_)).WillOnce(Return(Coords{ {0, 0}, {35, 8} }));
 
-	auto board = life::Create({}, generator);
+	auto board = life::Create({{""}, {}}, generator);
 
 	EXPECT_EQ(36, board->width());
 	EXPECT_EQ(9, board->height());
@@ -46,12 +46,22 @@ TEST_F(BoardCreation, create_trims_size_when_pattern_does_not_start_at_origin)
 	EXPECT_EQ(1, board->height());
 }
 
+TEST_F(BoardCreation, create_trims_size_when_pattern_is_offset)
+{
+	EXPECT_CALL(mockGenerator, create(_)).WillOnce(Return(Coords{ {0, 0}, {3, 4} }));
+
+	auto board = life::Create({{""}, {1, 2}}, generator);
+
+	EXPECT_EQ(4, board->width());
+	EXPECT_EQ(5, board->height());
+}
+
 TEST_F(BoardCreation, create_with_pattern)
 {
 	EXPECT_CALL(mockGenerator, create(_))
 		.WillOnce(Return(Coords{ {1, 0}, {2, 1}, {0, 2}, {1, 2}, {2, 2} }));
 
-	auto board = life::Create({}, generator);
+	auto board = life::Create({{""}, {}}, generator);
 
 	std::vector<std::pair<Coord, bool>> dataset = {
 		{{0, 0}, false},

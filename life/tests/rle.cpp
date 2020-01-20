f9d02c304fc967a5087aa19a07e8114e5ab3e689
Bruno Belarte <bruno.belarte@gmail.com>
2020-01-20 21:08:09 +0000

    Add eater pattern

--- life/tests/rle.cpp
+++ life/tests/rle.cpp
@@ -6,14 +6,35 @@
 const std::string Glider = R"(x = 3, y = 3
 bo$2bo$3o!)";
 
+const std::string Eater = R"(x = 9, y = 6
+3b2o2b2o$o3bo2b2o$4o$$2o$2o!)";
+
 using namespace life;
 
-TEST(FromRLE, create_with_pattern)
+TEST(FromRLE, create_with_pattern_glider)
 {
 	auto coords = FromRLE(Glider);
 
 	Coords expected = {
 		{1, 0}, {2, 1}, {0, 2}, {1, 2}, {2, 2}
+	};
+
+	EXPECT_EQ(expected.size(), coords.size());
+	for (size_t i=0; i<expected.size(); ++i) {
+		EXPECT_EQ(expected[i], coords[i]);
+	}
+}
+
+TEST(FromRLE, create_with_pattern_eater)
+{
+	auto coords = FromRLE(Eater);
+
+	Coords expected = {
+		{3, 0}, {4, 0}, {7, 0}, {8, 0},
+		{0, 1}, {4, 1}, {7, 1}, {8, 1},
+		{0, 2}, {1, 2}, {2, 2}, {3, 2},
+		{0, 4}, {1, 4},
+		{0, 5}, {1, 5}
 	};
 
 	EXPECT_EQ(expected.size(), coords.size());

e43ce17eae23298d33d96d4d4b74d4d603ec6685
Bruno Belarte <bruno.belarte@gmail.com>
2020-01-25 22:48:27 +0000

    Backend waits for client for shutdown

--- backend/app/main.cpp
+++ backend/app/main.cpp
@@ -1,4 +1,30 @@
+#include <boost/beast/websocket.hpp>
+
+using namespace boost::beast::websocket;
+
 int main(int, char**)
 {
-	return 0;
+	try {
+		auto const address = boost::asio::ip::make_address("127.0.0.1");
+		auto const port = static_cast<unsigned short>(8970);
+
+		boost::asio::io_context ioc{1};
+		boost::asio::ip::tcp::acceptor acceptor{ioc, {address, port}};
+		boost::asio::ip::tcp::socket socket{ioc};
+
+		acceptor.accept(socket);
+		boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws{std::move(socket)};
+		ws.accept();
+
+		boost::beast::multi_buffer buffer;
+		ws.read(buffer);
+		ws.text(ws.got_text());
+		ws.write(boost::asio::buffer("ctrl: shutdown"));
+		ws.close(boost::beast::websocket::close_code::normal);
+	} catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
--- system_tests/test_backend.py
+++ system_tests/test_backend.py
@@ -1,8 +1,20 @@
 import pytest
+
 import subprocess
+import asyncio
+import websockets
+
+async def exit():
+    uri = "ws://localhost:8970"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send('ctrl: exit')
+        ack = await websocket.recv()
+        assert ack.strip('\x00') == "ctrl: shutdown"
 
 def test_backend():
     process = subprocess.Popen(['../bin/app/life-calculator'])
-    process.wait()
 
+    asyncio.get_event_loop().run_until_complete(exit())
+
+    process.wait()
     assert process.returncode == 0

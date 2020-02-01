be9a62fb092cee445b46a1c2a06a9b83513a9cfb
Bruno Belarte <bruno.belarte@gmail.com>
2020-02-01 15:22:57 +0000

    Add Websocket class to wrap boost::beast

--- backend/app/main.cpp
+++ backend/app/main.cpp
@@ -1,26 +1,59 @@
+#include <boost/beast/core/buffers_to_string.hpp>
 #include <boost/beast/websocket.hpp>
 
 using namespace boost::beast::websocket;
 
-int main(int, char**)
+class Websocket
 {
-	try {
-		auto const address = boost::asio::ip::make_address("127.0.0.1");
-		auto const port = static_cast<unsigned short>(8970);
-
+public:
+	Websocket() {
+		const boost::asio::ip::address address = boost::asio::ip::make_address("127.0.0.1");
+		const unsigned short port = 8970;
 		boost::asio::io_context ioc{1};
 		boost::asio::ip::tcp::acceptor acceptor{ioc, {address, port}};
 		boost::asio::ip::tcp::socket socket{ioc};
-
 		acceptor.accept(socket);
-		boost::beast::websocket::stream<boost::asio::ip::tcp::socket> ws{std::move(socket)};
-		ws.accept();
+		m_websocket = std::make_unique<boost::beast::websocket::stream<boost::asio::ip::tcp::socket>>(std::move(socket));
+		m_websocket->accept();
+	}
 
-		boost::beast::multi_buffer buffer;
-		ws.read(buffer);
-		ws.text(ws.got_text());
-		ws.write(boost::asio::buffer("ctrl: shutdown"));
-		ws.close(boost::beast::websocket::close_code::normal);
+	void run() {
+		std::map<std::string, std::string> communications{
+			{"ctrl: exit", "ctrl: shutdown"},
+			{"app: expr=0", "app: nothing"},
+			{"app: expr=1", "app: data: x = 3, y = 3\nbo$2bo$3o!"}
+		};
+
+		while(true) {
+			boost::beast::multi_buffer buffer;
+			m_websocket->read(buffer);
+			std::string msg = boost::beast::buffers_to_string(buffer.data());
+
+			auto check = communications.find(msg);
+			if (check != communications.end()) {
+				std::cout << "[recv] " << msg << std::endl;
+				std::cout << "[send] " << check->second << std::endl;
+				m_websocket->write(boost::asio::buffer(check->second));
+			} else {
+				m_websocket->write(boost::asio::buffer("ctrl: unknown message"));
+				std::cout << "[send] " << "ctrl: unknown message" << std::endl;
+			}
+
+			if (msg == "ctrl: exit") {
+				break;
+			}
+		}
+	}
+
+private:
+	std::unique_ptr<boost::beast::websocket::stream<boost::asio::ip::tcp::socket>> m_websocket;
+};
+
+int main(int, char**)
+{
+	try {
+		Websocket ws;
+		ws.run();
 	} catch (const std::exception& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		return EXIT_FAILURE;
--- system_tests/test_backend.py
+++ system_tests/test_backend.py
@@ -4,17 +4,56 @@ import subprocess
 import asyncio
 import websockets
 
-async def exit():
-    uri = "ws://localhost:8970"
-    async with websockets.connect(uri) as websocket:
-        await websocket.send('ctrl: exit')
-        ack = await websocket.recv()
-        assert ack.strip('\x00') == "ctrl: shutdown"
+@pytest.fixture
+def app():
+    process = subprocess.Popen(['../bin/app/life-calculator'])
+    yield process
+    process.wait()
+    assert process.returncode == 0
 
-def test_backend():
+def test_connect_and_disconnect():
     process = subprocess.Popen(['../bin/app/life-calculator'])
 
-    asyncio.get_event_loop().run_until_complete(exit())
+    async def exit():
+        uri = "ws://localhost:8970"
+        async with websockets.connect(uri) as websocket:
+            await websocket.send('ctrl: exit')
+            ack = await websocket.recv()
+            assert ack.strip('\x00') == "ctrl: shutdown"
+
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(exit())
+    loop.close()
 
     process.wait()
     assert process.returncode == 0
+
+def test_setup_with_0_returns_empty_board(app):
+    async def setup_0():
+        uri = "ws://localhost:8970"
+        async with websockets.connect(uri) as websocket:
+            await websocket.send('app: expr=0')
+            ack = await websocket.recv()
+            assert ack.strip('\x00') == "app: nothing"
+            await websocket.send('ctrl: exit')
+            ack = await websocket.recv()
+            assert ack.strip('\x00') == "ctrl: shutdown"
+
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(setup_0())
+    loop.close()
+
+def test_setup_with_1_returns_one_glider(app):
+    async def setup_1():
+        uri = "ws://localhost:8970"
+        async with websockets.connect(uri) as websocket:
+            await websocket.send('app: expr=1')
+            ack = await websocket.recv()
+            assert ack.strip('\x00') == "app: data: x = 3, y = 3\nbo$2bo$3o!"
+            await websocket.send('ctrl: exit')
+            ack = await websocket.recv()
+            assert ack.strip('\x00') == "ctrl: shutdown"
+
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(setup_1())
+    loop.close()

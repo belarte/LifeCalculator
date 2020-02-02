0235ed135b374e06a474bc98da64c1f9a75db020
Bruno Belarte <bruno.belarte@gmail.com>
2020-02-02 11:48:24 +0000

    Extract processing of app specific message into a function

--- backend/app/main.cpp
+++ backend/app/main.cpp
@@ -6,7 +6,9 @@ using namespace boost::beast::websocket;
 class Websocket
 {
 public:
-	Websocket() {
+	Websocket() :
+		m_continue{true}
+	{
 		const boost::asio::ip::address address = boost::asio::ip::make_address("127.0.0.1");
 		const unsigned short port = 8970;
 		boost::asio::io_context ioc{1};
@@ -18,35 +20,44 @@ public:
 	}
 
 	void run() {
-		std::map<std::string, std::string> communications{
-			{"ctrl: exit", "ctrl: shutdown"},
-			{"app: expr=0", "app: nothing"},
-			{"app: expr=1", "app: data: x = 3, y = 3\nbo$2bo$3o!"}
-		};
-
-		while(true) {
+		while(m_continue) {
 			boost::beast::multi_buffer buffer;
 			m_websocket->read(buffer);
 			std::string msg = boost::beast::buffers_to_string(buffer.data());
+			std::cout << "[recv] " << msg << std::endl;
 
-			auto check = communications.find(msg);
-			if (check != communications.end()) {
-				std::cout << "[recv] " << msg << std::endl;
-				std::cout << "[send] " << check->second << std::endl;
-				m_websocket->write(boost::asio::buffer(check->second));
-			} else {
-				m_websocket->write(boost::asio::buffer("ctrl: unknown message"));
-				std::cout << "[send] " << "ctrl: unknown message" << std::endl;
-			}
-
+			std::string reply;
 			if (msg == "ctrl: exit") {
-				break;
+				reply = "ctrl: shutdown";
+				m_continue = false;
+			} else if (msg.rfind("app: ", 0) == 0) {
+				reply = processAppMessages(msg);
+			} else {
+				reply = "ctrl: unknown message";
 			}
+
+			std::cout << "[send] " << reply << std::endl;
+			m_websocket->write(boost::asio::buffer(reply));
 		}
 	}
 
 private:
+	const std::string processAppMessages(const std::string& msg) {
+		std::map<std::string, std::string> communications{
+			{"app: expr=0", "app: nothing"},
+			{"app: expr=1", "app: data: x = 3, y = 3\nbo$2bo$3o!"}
+		};
+
+		auto check = communications.find(msg);
+		if (check != communications.end()) {
+			return check->second;
+		}
+
+		return "";
+	}
+
 	std::unique_ptr<boost::beast::websocket::stream<boost::asio::ip::tcp::socket>> m_websocket;
+	bool m_continue;
 };
 
 int main(int, char**)

c0ae030ae2586c18e30eeb7e8f1ca09926a5519c
Bruno Belarte <bruno.belarte@gmail.com>
2020-02-08 21:42:09 +0000

    Add comment to illustrate RLE patterns

--- backend/life/rle.h
+++ backend/life/rle.h
@@ -8,9 +8,18 @@ namespace life {
 
 namespace rle {
 
+// -o-
+// --o
+// ooo
 const std::string Glider = R"(x = 3, y = 3
 bo$2bo$3o!)";
 
+// ---oo--oo
+// o---o--oo
+// oooo-----
+// ---------
+// oo-------
+// oo-------
 const std::string Eater = R"(x = 9, y = 6
 3b2o2b2o$o3bo2b2o$4o$$2o$2o!)";
 

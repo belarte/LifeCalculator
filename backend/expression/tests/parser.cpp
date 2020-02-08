5d6fffbb5d512d318895bfc438ff2069a3709e9c
Bruno Belarte <bruno.belarte@gmail.com>
2020-02-08 21:21:12 +0000

    Test parser returns const expression for numbers

--- backend/expression/tests/parser.cpp
+++ backend/expression/tests/parser.cpp
@@ -1,5 +1,7 @@
 #include "../parser.h"
 
+#include <typeinfo>
+
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
@@ -9,4 +11,11 @@ using namespace testing;
 TEST(ParserThrowsException, when_number_is_larger_than_8_bits)
 {
 	EXPECT_THROW(Parser{}.parse("256"), InvalidNumber);
+}
+
+TEST(ParserReturns, concrete_expression_for_number)
+{
+	auto expr = Parser{}.parse("123");
+
+	EXPECT_EQ(typeid(*expr.get()), typeid(ConstExpression));
 }

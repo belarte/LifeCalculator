43c909533d6069031fab40cdf07005b923ddd7b1
Bruno Belarte <bruno.belarte@gmail.com>
2020-02-08 21:43:14 +0000

    Fix type in test fixture

--- backend/expression/tests/expression.cpp
+++ backend/expression/tests/expression.cpp
@@ -7,7 +7,7 @@
 using namespace expression;
 using namespace testing;
 
-TEST(ConcreteExpressionEvatuatesTo, empty_inputs_when_input_is_zero)
+TEST(ConstExpressionEvatuatesTo, empty_inputs_when_input_is_zero)
 {
 	auto expr = ConstExpression{0};
 	auto inputs = expr.evaluate();
@@ -15,7 +15,7 @@ TEST(ConcreteExpressionEvatuatesTo, empty_inputs_when_input_is_zero)
 	EXPECT_THAT(inputs, IsEmpty());
 }
 
-TEST(ConcreteExpressionEvatuatesTo, input_with_one_glider_and_no_offset)
+TEST(ConstExpressionEvatuatesTo, input_with_one_glider_and_no_offset)
 {
 	auto expr = ConstExpression{1};
 	auto inputs = expr.evaluate();
@@ -26,7 +26,7 @@ TEST(ConcreteExpressionEvatuatesTo, input_with_one_glider_and_no_offset)
 	EXPECT_THAT(inputs[0].offsets[0], Eq(life::Coord{0, 0}));
 }
 
-TEST(ConcreteExpressionEvatuatesTo, correct_input_for_large_number)
+TEST(ConstExpressionEvatuatesTo, correct_input_for_large_number)
 {
 	auto expr = ConstExpression{210};
 	auto inputs = expr.evaluate();

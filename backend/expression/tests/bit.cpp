08a30f9cbf91d1971db053dbcaca617435cff374
Bruno Belarte <bruno.belarte@gmail.com>
2020-02-15 17:49:51 +0000

    Remove unnecessary dependency to rle

--- backend/expression/tests/bit.cpp
+++ backend/expression/tests/bit.cpp
@@ -1,5 +1,4 @@
 #include "../bit.h"
-#include "life/rle.h"
 #include "life/types.h"
 
 #include "gtest/gtest.h"
@@ -10,39 +9,39 @@ using namespace testing;
 
 TEST(BitConstructs, a_bit_with_inputs)
 {
-	Bit bit{ life::Inputs{ { life::rle::Glider, life::Coords{ {0,0} } } } };
+	Bit bit{ life::Inputs{ { "a", life::Coords{ {0,0} } } } };
 
-	EXPECT_THAT(bit.output(), Eq(life::Inputs{ { life::rle::Glider, life::Coords{ {0,0} } } }));
+	EXPECT_THAT(bit.output(), Eq(life::Inputs{ { "a", life::Coords{ {0,0} } } }));
 }
 
 TEST(BitConstructs, a_bit_from_two_bits)
 {
 	Bit left{ life::Inputs{
-		{ life::rle::Glider, life::Coords{ {0, 0} } },
-		{ life::rle::Eater, life::Coords{ {5, 5} } }
+		{ "a", life::Coords{ {0, 0} } },
+		{ "b", life::Coords{ {5, 5} } }
 	} };
 
 	Bit right{ life::Inputs{
-		{ life::rle::Glider, life::Coords{ {1, 1}, {2, 2} } }
+		{ "a", life::Coords{ {1, 1}, {2, 2} } }
 	} };
 
 	EXPECT_THAT(Bit(left, right), Eq( Bit{ life::Inputs{
-		{ life::rle::Glider, life::Coords{ {0, 0}, {1, 1}, {2, 2} } },
-		{ life::rle::Eater, life::Coords{ {5, 5} } }
+		{ "a", life::Coords{ {0, 0}, {1, 1}, {2, 2} } },
+		{ "b", life::Coords{ {5, 5} } }
 	} }));
 }
 
 TEST(BitManipulation, offset_propagates_to_output)
 {
 	Bit input{ life::Inputs{
-		{ life::rle::Glider, life::Coords{ {0, 0}, {1, 0} } },
-		{ life::rle::Eater, life::Coords{ {5, 5}, {10, 10} } }
+		{ "a", life::Coords{ {0, 0}, {1, 0} } },
+		{ "b", life::Coords{ {5, 5}, {10, 10} } }
 	} };
 
 	input.offset({1, 2});
 
 	EXPECT_THAT(input.output(), Eq(life::Inputs{
-		{ life::rle::Glider, life::Coords{ {1, 2}, {2, 2} } },
-		{ life::rle::Eater, life::Coords{ {6, 7}, {11, 12} } }
+		{ "a", life::Coords{ {1, 2}, {2, 2} } },
+		{ "b", life::Coords{ {6, 7}, {11, 12} } }
 	}));
 }
